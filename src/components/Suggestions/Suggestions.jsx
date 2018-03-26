f05be28e0d92eccc42fbab4b1d998a7d1bc174a5
luigi055 <luigi5021@gmail.com>
2018-03-25 21:52:19 -0400

    :raised_hands: Installing and passing data to the autocompletion

--- src/components/Suggestions/Suggestions.jsx
+++ src/components/Suggestions/Suggestions.jsx
@@ -6,50 +6,71 @@ type Props = {
   searchTerm: string,
   maxWidth: string,
   updateLocalState: Function,
-  open: boolean
+  updateParentState: Function,
+  open: boolean,
+  autoCompleteItems: Array<string>,
+  includeSearchTerm: boolean,
+  maxSuggests: number
 };
 
 const Suggestions = ({
   searchTerm,
   maxWidth,
+  maxSuggests,
   updateLocalState,
-  open
-}: Props) => (
-  <ItemSuggestions
-    open={open && searchTerm.length > 0 ? open : false}
-    maxWidth={maxWidth}
-  >
-    <ul className="suggestions-list">
-      <li
-        onClick={Suggestions.selectCompletion(updateLocalState)}
-        role="presentation"
-      >
-        Item 1
-      </li>
-      <li
-        onClick={Suggestions.selectCompletion(updateLocalState)}
-        role="presentation"
-      >
-        Item 2
-      </li>
-      <li
-        onClick={Suggestions.selectCompletion(updateLocalState)}
-        role="presentation"
-      >
-        Item 3
-      </li>
-    </ul>
-  </ItemSuggestions>
-);
+  updateParentState,
+  open,
+  autoCompleteItems,
+  includeSearchTerm
+}: Props) => {
+  const AutoCompleteList = autoCompleteItems
+    .filter(autoCompleteItem => {
+      // If the search item is include within the array item this will show
+      // all the options which includes the search items
+      if (includeSearchTerm) {
+        return (
+          autoCompleteItem.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
+        );
+      }
+      // By default the autocomplete input will show only the options which
+      // search term starts and includes with the search term
+      return (
+        autoCompleteItem.toLowerCase().search(searchTerm.toLowerCase()) === 0
+      );
+    })
+    .splice(0, maxSuggests);
+  return (
+    <ItemSuggestions
+      open={open && searchTerm.length > 0 ? open : false}
+      maxWidth={maxWidth}
+    >
+      <ul className="suggestions-list">
+        {AutoCompleteList.map(autoCompleteItem => (
+          <li
+            key={autoCompleteItem}
+            onClick={Suggestions.selectCompletion(
+              updateLocalState,
+              updateParentState
+            )}
+            role="presentation"
+          >
+            {autoCompleteItem}
+          </li>
+        ))}
+      </ul>
+    </ItemSuggestions>
+  );
+};
 
-Suggestions.selectCompletion = (updateLocalState: Function) => (
-  event: SyntheticMouseEvent<HTMLLiElement>
-) => {
+Suggestions.selectCompletion = (
+  updateLocalState: Function,
+  updateParentState: Function
+) => (event: SyntheticMouseEvent<HTMLLiElement>) => {
   updateLocalState({
     searchTerm: event.currentTarget.textContent,
-    open: false,
-    suggestedItems: []
+    open: false
   });
+  updateParentState(event.target.textContent);
 };
 
 export default Suggestions;
